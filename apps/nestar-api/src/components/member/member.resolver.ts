1dc5cee4a68b0873e5fb826ba555679ab5ffe6c1
diyorbekMIT <diyorbekjon2202@gmail.com>
2025-06-23 01:07:11 +0900

    feat: authorization is finished

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -2,9 +2,14 @@ import { Args, Resolver } from "@nestjs/graphql";
 import { AppService } from "../../app.service";
 import { Query, Mutation } from "@nestjs/graphql";
 import { MemberService } from "./member.service";
-import { InternalServerErrorException, UsePipes, ValidationPipe } from "@nestjs/common";
+import { InternalServerErrorException, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
 import { LoginInput, MemberInput } from "../../libs/dto/member/member.input";
 import { Member } from "../../libs/dto/member/member";
+import { AuthGuard } from "../auth/guards/auth.guard";
+import { AuthMember } from "../auth/decorators/authMember.decorator";
+import { Roles } from "../auth/decorators/roles.decorator";
+import { RolesGuard } from "../auth/guards/roles.guard";
+import { MemberType } from "../../libs/enums/member.enum";
 
 @Resolver()
 @UsePipes(ValidationPipe)
@@ -30,11 +35,28 @@ export class MemberResolver {
         return this.memberService.login(input)
     }x
 
+    @UseGuards(AuthGuard)
     @Mutation(() => String)
-    public async updateMember(): Promise<string> {
+    public async updateMember(@AuthMember() authMember: Member): Promise<string> {
         console.log("updateMember: signup");
         return this.memberService.updateMember()
     }
+
+    @UseGuards(AuthGuard)
+    @Query(() => String)
+    public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
+        console.log("checkAuth: checkAuth");
+        console.log(`memberNick, ${memberNick}`)
+        return `hello, ${memberNick}`
+    }
+    @Roles(MemberType.USER, MemberType.AGENT)
+    @UseGuards(RolesGuard)
+    @Query(() => String)
+    public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+        console.log("checkAuthRoles: checkAuthRolese");
+        return `hello, ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`
+    }
+
 
     @Query(() => String)
     public async getMember(): Promise<string> {
